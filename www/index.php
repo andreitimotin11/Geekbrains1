b9e9d0cc44c9d577bfc314a2c80bed9a15d1d841
Andrei Timotin <andreitimotin11@gmail.com>
2016-11-08 14:27:27 +0200

    add function dateInRu

--- www/index.php
+++ www/index.php
@@ -9,14 +9,58 @@ function fibonacci($x)
 {
 	if($x===0){
 		return 0;
-	}
+	};
 	if($x==1) {
 		return 1;
 	}else{
-		return fibonacci($x-1)+ fibonacci($x-2);
-	}
+		return (fibonacci($x-1) + fibonacci($x-2));
+	};
 }
 
+/*
+ * Напишите функцию, которая вычисляет доход по
+вкладу. В качестве аргумента принимается сумма
+вклада, срок в месяцах, годовой процент.
+Возвращается сумма вклада по окончанию срока.
+ */
+function dohod($suma, $termen, $procentAnual)
+{
+	$procentLunar = $procentAnual / 12;
+	$suma += $suma * $procentLunar * $termen;
+	return $suma;
+}
+function dohodComplex($suma, $termen, $procentAnual)
+{
+	$procentLunar = $procentAnual / 12;
+	if($termen == 1){
+		$suma =  $suma+ $suma*$procentLunar;
+	}else{
+		$suma = dohodComplex($suma, $termen - 1, $procentAnual)*$procentLunar + dohodComplex($suma, $termen - 1, $procentAnual);
+	}
+	return $suma;
+
+}
+/*
+ * Напишите функцию, которая принимает на вход два
+аргумента – число (1..31) и номер месяца (1..12).
+Возвращает правильно сформированную дату на
+русском языке. Например: «1 января» или «9 мая»
+ */
+$months[] = array('1' => 'января',
+                  '1' =>'февраля',
+                  'марта',
+                  'апреля',
+                  'мая',
+                  'июня',
+                  'июля',
+                  'августа',
+                  'сентября',
+                  'октября',
+                  'ноября',
+                  'декабря');
+function dateInRu($day, $month){
+
+}
 ?>
 
 <html>
@@ -27,6 +71,6 @@ function fibonacci($x)
 </head>
 <body>
 <h1>Hello world!</h1>
-<h2><?php echo fibonacci(4);?></h2>
+<h2><?php echo var_dump($months);?></h2>
 </body>
 </html>
