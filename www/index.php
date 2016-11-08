6bbb463dc550136425181c3bf8f82271cb29d9ce
Andrei Timotin <andreitimotin11@gmail.com>
2016-11-08 13:33:07 +0200

    add function fibonacci

--- www/index.php
+++ www/index.php
@@ -1,3 +1,24 @@
+<?php
+/*
+ * Изучите понятие «рекурсия», составьте рекурсивную
+функцию вычисления чисел Фибоначчи, проверьте ее
+работу
+F0 = 0, F1 = 1, Fi = F(i-1) + F(i-2), pentru i>=2
+ */
+function fibonacci($x)
+{
+	if($x===0){
+		return 0;
+	}
+	if($x==1) {
+		return 1;
+	}else{
+		return fibonacci($x-1)+ fibonacci($x-2);
+	}
+}
+
+?>
+
 <html>
 <head>
 	<title>
@@ -6,5 +27,6 @@
 </head>
 <body>
 <h1>Hello world!</h1>
+<h2><?php echo fibonacci(4);?></h2>
 </body>
 </html>
